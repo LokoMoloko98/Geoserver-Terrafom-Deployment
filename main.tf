aee52488ac2c80170335aa58df862b58758637a8
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-22 16:15:38 +0200

    Add multiple geoserver modules with distinct networking configurations

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
-module "networking" {
+module "networking1" {
   source                 = "./networking"
   region                 = var.region
-  public_subnet_az1_cidr = var.public_subnet_az1_cidr
-  vpc_cidr               = var.vpc_cidr
-  project_name           = var.project_name
+  public_subnet_az1_cidr = "10.31.1.0/24"
+  vpc_cidr               = "10.31.0.0/16"
+  project_name           = "geoserver1"
+}
+
+module "networking2" {
+  source                 = "./networking"
+  region                 = var.region
+  public_subnet_az1_cidr = "10.30.1.0/24"
+  vpc_cidr               = "10.30.0.0/16"
+  project_name           = "geoserver2"
 }
 
 module "iam" {
@@ -12,16 +20,29 @@ module "iam" {
   region       = var.region
 }
 
-module "geoserver" {
+module "geoserver1" {
   source            = "./compute"
-  security_group_id = module.networking.security_group_id
+  security_group_id = module.networking1.security_group_id
   instance_profile  = module.iam.instance_profile
-  subnet_id         = module.networking.subnet_id
+  subnet_id         = module.networking1.subnet_id
   ssh_key_pair      = var.ssh_key_pair
   instance_type     = var.instance_type
   ami_id            = var.ami_id
   host_os           = var.host_os
   region            = var.region
-  project_name      = var.project_name
+  project_name      = "geoserver1"
 
+}
+
+module "geoserver2" {
+  source            = "./compute"
+  security_group_id = module.networking2.security_group_id
+  instance_profile  = module.iam.instance_profile
+  subnet_id         = module.networking2.subnet_id
+  ssh_key_pair      = var.ssh_key_pair
+  instance_type     = var.instance_type
+  ami_id            = var.ami_id
+  host_os           = var.host_os
+  region            = var.region
+  project_name      = "geoserver2"
 }
